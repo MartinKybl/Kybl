3339e59d6db8f4f399277c9c5f3766dbf928a35b
Martin Robb <144167767+MartinKybl@users.noreply.github.com>
2023-10-29 18:21:04 +0100

    Add files via upload

--- Morse.py
+++ Morse.py
@@ -1,8 +1,22 @@
 from machine import Pin
 import time
+#project done on Raspberry Pico W
+#defining output led
 
 led = Pin("LED", Pin.OUT)
 led.off()
+
+#defining the time of a dot and a comma
+
+tdot = 200
+tcomma = 3*tdot
+
+#defining the time between words and letters
+
+tword = 7*tdot
+tletter = tcomma
+
+#morse dictionary
 
 morseovka_dic = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
@@ -21,46 +35,55 @@ morseovka_dic = { 'A':'.-', 'B':'-...',
                     '(':'-.--.', ')':'-.--.-',' ':'/'
                     }
 
+#input data
+
 vstup = input()
 seznam = []
+
+#making every letter capital baby
 
 for letter in vstup:
     seznam.append(letter.upper())
 
 i = 0
 while(i<len(seznam)):
+    
+    # spliting morse letters into individual symbol
+
     print(morseovka_dic[seznam[i]])
     x = list(morseovka_dic[seznam[i]])
-    
+    print(x)
     z = 0
+    
+    #blinking for daddy
+    
     for n in range(len(x)):
         y = x[z]
         
         if y ==".":
             led.on()
             
-            time.sleep_ms(500)
+            time.sleep_ms(tdot)
             
             led.off()
-            time.sleep_ms(1000)
+            time.sleep_ms(tletter)
         else: 
             if y =="-":
                 led.on()
                 
-                time.sleep_ms(1200)
+                time.sleep_ms(tcomma)
                 
                 led.off()
-                time.sleep_ms(1000)
+                time.sleep_ms(tletter)
             else:
                 if y =="/":
                     led.off
-                    time.sleep_ms(1200)
-                else:
-                    print("prace dokoncena sefe")
-
+                    time.sleep_ms(tword)
+                
         z = z + 1    
     
     i = i + 1
 
+print("translation complete")
     
 
